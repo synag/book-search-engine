3f85405ab847fd061981e223e7b4eac1183d5cce
Carl Benbow <cbenbow@synergisticagility.com>
2021-07-22 22:16:50 -0400

    initial files and updated server file to use graphql

---
+++ Develop/server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+// Import the ApolloServer class
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// Update Express.js to use Apollo server features
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
