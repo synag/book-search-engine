c5b2ef452be3ac4d0e5c2a249b0c29a035e5590f
Carl Benbow <cbenbow@synergisticagility.com>
2021-07-22 22:31:24 -0400

    updated the server files

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -30,8 +30,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
